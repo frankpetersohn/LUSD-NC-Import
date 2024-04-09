b52610da9bdd427cd1641fd231fb68cb02c76ff7
frank petersohn <petersohn@berufsschule-schwalmstadt.de>
2024-04-09 17:10:06 +0200

    importconfit hinzugefügt

---
+++ importConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$importConfig = [
+    'Lehrer_Quota' => '10 GB', //Speicherkapazität mit Leerzeichen zwischen Wert und Einheit
+    'Schueler_Quota' => '2 GB', //Speicherkapazität mit Leerzeichen zwischen Wert und Einheit
+    'logFile' => 'importLog_' . date("y-m-d") . '.txt',
+    'nextcloudPath' => '/var/www/html',
+    'AdminUser' => 'Admin',
+];
